7357df25c5e09720189ee46271561a82ac1264fd
Thenea <thenea@iMac4.local>
2018-11-08 12:45:38 -0500

    This is hardgit add assignment_eight.py git add assignment_eight.py git add assignment_eight.py

---
+++ assignment_eight.py
@@ -0,0 +1,11 @@
+# Thenea Sun
+# Nov. 8th
+# This program is a simulator of calculators. It used the program of Graphical User Interfaces, in order to create a
+# real calculator.
+
+import tkinter
+
+root = tkinter.Tk()
+
+root.title("Thenea's Calculator")
+
