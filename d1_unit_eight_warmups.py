d52023c20d74a5c38a72444f7a74220a7aaf5af8
Thenea <thenea@iMac4.local>
2018-11-08 12:26:10 -0500

    Adding variables

--- d1_unit_eight_warmups.py
+++ d1_unit_eight_warmups.py
@@ -8,13 +8,17 @@ root.title("Temperature Converter")
 fahrenheitLabel = tkinter.Label(root, text="Fahrenheit:")
 fahrenheitLabel.grid(column=1, row=1)
 
+fahrenheitValue = tkinter.StringVar()
+celsiusValue = tkinter.StringVar()
+
 celsiusLabel = tkinter.Label(root, text="Celsius:")
 celsiusLabel.grid(column=1, row=2)
 
-fahrenheitEntry = tkinter.Entry(root)
+fahrenheitEntry = tkinter.Entry(root, textvariable=fahrenheitValue)
 fahrenheitEntry.grid(column=2, row=1)
 
-resultLabel = tkinter.Label(root)
+resultLabel = tkinter.Label(root, textvariable=celsiusValue)
 resultLabel.grid(column=2, row=2)
+
 
 root.mainloop()
