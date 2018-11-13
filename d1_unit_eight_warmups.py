17bf0cbc57eefc3f1c27f04e5f5961ed2ef33405
Thenea <thenea@iMac4.local>
2018-11-13 09:19:46 -0500

    Clean up GUI

--- d1_unit_eight_warmups.py
+++ d1_unit_eight_warmups.py
@@ -2,8 +2,14 @@
 import tkinter
 
 root = tkinter.Tk()
-
 root.title("Temperature Converter")
+
+
+def f_to_c():
+    f_value = int(fahrenheitValue.get())
+    c_value = 5/9 * (f_value - 32)
+    celsiusValue.set(str(c_value))
+
 
 fahrenheitLabel = tkinter.Label(root, text="Fahrenheit:")
 fahrenheitLabel.grid(column=1, row=1)
@@ -18,7 +24,11 @@ fahrenheitEntry = tkinter.Entry(root, textvariable=fahrenheitValue)
 fahrenheitEntry.grid(column=2, row=1)
 
 resultLabel = tkinter.Label(root, textvariable=celsiusValue)
-resultLabel.grid(column=2, row=2)
+resultLabel.grid(column=2, row=2, sticky="W")
+
+
+convert_botton = tkinter.Botton(root, text="Convert", command=f_to_c())
+convert_botton.grid(row=3, column=1, colunmspan=2)
 
 
 root.mainloop()
