ee4d1611742dc31a4383099b55d0cc2e543b3823
Thenea <thenea@iMac4.local>
2018-11-14 11:10:53 -0500

    Almost done, finished the adjustment for equal and all the signs.

--- assignment_eight.py
+++ assignment_eight.py
@@ -85,11 +85,17 @@ def divide():
 
 def equal():
     equal = answer.get()
-    answer.set(equal + "=")
+    answer.set(eval(equal))
+# tried to make equal work, and I got the function above to create this whole progress
 
 
 def AC():
     answer.set("")
+
+
+def dots():
+    dot = answer.get()
+    answer.set(dot + ".")
 
 
 answer = tkinter.StringVar()
@@ -97,52 +103,55 @@ answer = tkinter.StringVar()
 LabelEntry = tkinter.Entry(root, textvariable=answer)
 LabelEntry.grid(row=2, column=1, columnspan=4)
 
-Label1 = tkinter.Button(root, text="1", command=one)
+Label1 = tkinter.Button(root, text="   1   ", command=one)
 Label1.grid(row=4, column=1)
 
-Label2 = tkinter.Button(root, text="2", command=two)
+Label2 = tkinter.Button(root, text="   2   ", command=two)
 Label2.grid(row=4, column=2)
 
-Label3 = tkinter.Button(root, text="3", command=three)
+Label3 = tkinter.Button(root, text="   3   ", command=three)
 Label3.grid(row=4, column=3)
 
-Label4 = tkinter.Button(root, text="4", command=four)
+Label4 = tkinter.Button(root, text="   4   ", command=four)
 Label4.grid(row=5, column=1)
 
-Label5 = tkinter.Button(root, text="5", command=five)
+Label5 = tkinter.Button(root, text="   5   ", command=five)
 Label5.grid(row=5, column=2)
 
-Label6 = tkinter.Button(root, text="6", command=six)
+Label6 = tkinter.Button(root, text="   6   ", command=six)
 Label6.grid(row=5, column=3)
 
-Label7 = tkinter.Button(root, text="7", command=seven)
+Label7 = tkinter.Button(root, text="   7   ", command=seven)
 Label7.grid(row=6, column=1)
 
-Label8 = tkinter.Button(root, text="8", command=eight)
+Label8 = tkinter.Button(root, text="   8   ", command=eight)
 Label8.grid(row=6, column=2)
 
-Label9 = tkinter.Button(root, text="9", command=nine)
+Label9 = tkinter.Button(root, text="   9   ", command=nine)
 Label9.grid(row=6, column=3)
 
-Label0 = tkinter.Button(root, text="0", command=zero)
+Label0 = tkinter.Button(root, text="         0          ", command=zero)
 Label0.grid(row=7, column=1, columnspan=2)
 
 Label_clear = tkinter.Button(root, text="AC/C", command=AC)
 Label_clear.grid(row=3, column=1)
 
-Label_equal = tkinter.Button(root, text="=", command=equal)
-Label_equal.grid(row=7, column=3)
+Label_equal = tkinter.Button(root, text="  =  ", command=equal)
+Label_equal.grid(row=7, column=4)
 
-Label_divide = tkinter.Button(root, text="/", comman=divide)
-Label_divide.grid(row=7, column=4)
+Label_divide = tkinter.Button(root, text="  /  ", comman=divide)
+Label_divide.grid(row=6, column=4)
 
-Label_times = tkinter.Button(root, text="*", command=times)
-Label_times.grid(row=6, column=4)
+Label_times = tkinter.Button(root, text="  *  ", command=times)
+Label_times.grid(row=5, column=4)
 
-Label_plus = tkinter.Button(root, text="+", command=plus)
-Label_plus.grid(row=4, column=4)
+Label_plus = tkinter.Button(root, text="  +  ", command=plus)
+Label_plus.grid(row=3, column=4)
 
-Label_minus = tkinter.Button(root, text="-", command=minus)
-Label_minus.grid(row=5, column=4)
+Label_minus = tkinter.Button(root, text="  -  ", command=minus)
+Label_minus.grid(row=4, column=4)
+
+Label_dots = tkinter.Button(root, text="    .   ", command=dots)
+Label_dots.grid(row=7, column=3)
 
 root.mainloop()
